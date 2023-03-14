fa66f41dd929e4c15db98f79ffdac21dbdd565f3
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-03-14 09:02:07 +0530

    Student

---
+++ src/main/java/com/bootcoding/java/oops/exercise1/Student.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.oops.exercise1;
+
+public class Student {
+    int rollno ;
+    String Name;
+    String Address;
+    double Percentage;
+    public void print() {
+        System.out.print("rollno = "+rollno);
+        System.out.println("Name = "+Name);
+        System.out.println("Address = "+Address);
+        System.out.println("Percentage = "+Percentage);
+    }
+
+        }
+
