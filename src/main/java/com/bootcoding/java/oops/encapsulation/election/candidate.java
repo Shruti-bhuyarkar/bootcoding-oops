bc1a3879a2c93dc21bf6d9e418e600d0fd7b3064
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-04-22 23:24:06 +0530

    candidate

---
+++ src/main/java/com/bootcoding/java/oops/encapsulation/election/candidate.java
@@ -0,0 +1,40 @@
+package com.bootcoding.java.oops.encapsulation.election;
+
+public class candidate {
+    public String name;
+    public int age;
+    public String education;
+   // public String panelname;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+//    public String getPanelname() {
+//        return panelname;
+//    }
+//
+//    public void setPanelname(String panelname) {
+//        this.panelname = panelname;
+//    }
+}
