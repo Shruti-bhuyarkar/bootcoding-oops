fe54fff7d314ecf72a81ecbf1dd1599b315da910
Shruti-bhuyarkar <BHUYARKARSHRUTI4@GMAIL.COM>
2023-04-29 20:16:43 +0530

    Banner

---
+++ src/main/java/com/bootcoding/java/oops/inheritance/banner/Banner.java
@@ -0,0 +1,19 @@
+package com.bootcoding.java.oops.inheritance.banner;
+
+class Banner {
+    String colour;
+    String shape;
+    String matter;
+    String type;
+    double price;
+    double dimension;
+    boolean lights;
+
+     public static void createBanner(){
+          System.out.println("Banner has been created....");
+
+    }
+    public static void displayBanner(){
+        System.out.println("Banner has been printed...");
+    }
+}
